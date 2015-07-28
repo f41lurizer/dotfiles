9634c050eb93647d666f4ca6f68cdbf4ac68e6b6
f41lurizer <forumf41l@gmail.com>
2015-07-27 21:36:11 -0500

    updated vimcompile

--- vim/vimcompile.sh
+++ vim/vimcompile.sh
@@ -2,3 +2,6 @@ sudo apt-get install liblua5.1-dev luajit libluajit-5.1
 ./configure --with-compiledby=f41lurizer --with-features=huge --enable-multibyte --enable-rubyinterp --enable-pythoninterp --enable-python3interp --enable-luainterp --with-lua-prefix=/usr/include/lua5.1
 make
 sudo install
+
+//good installation instructions for lua
+https://otland.net/threads/configure-error-lua-header-not-found.116586/
