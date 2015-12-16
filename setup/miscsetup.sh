f0b82d9df04230655a62b8a60b7dff24003e78c1
kasldfh <forumf41l@gmail.com>
2015-12-16 00:15:43 -0600

    added tmux configs

--- setup/miscsetup.sh
+++ setup/miscsetup.sh
@@ -3,6 +3,10 @@ cwd=$(pwd)
 gtkpath="$cwd/../gtk/gtk.css"
 sshpath="$cwd/../ssh/config"
 
+#set up tmux
+rm ~/.tmux.conf
+ln -s $cwd/../tmux/.tmux.conf ~/.tmux.conf
+
 #set up gtk config (for gnome terminal)
 rm -rf ~/.config/gtk-3.0/gtk.css
 ln -s $gtkpath ~/.config/gtk-3.0/gtk.css
