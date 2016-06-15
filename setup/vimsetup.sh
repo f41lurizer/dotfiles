2b7a3af3cf6100fa46b37c3c01025a846b342bde
kasldfh <forumf41l@gmail.com>
2016-06-15 06:58:34 -0700

    automatically install plugins on setup

--- setup/vimsetup.sh
+++ setup/vimsetup.sh
@@ -7,6 +7,7 @@ rm -rf ~/.vim ~/.vimrc
 ln -s $vimPath ~/.vim
 ln -s $vimrcPath ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall!
 #create directories for vim (undo, swap, prev)
 mkdir ~/.vim/undo
 mkdir ~/.vim/swap
