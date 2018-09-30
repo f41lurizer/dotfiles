69258cd62a1e73cb53336cca967310456c00c1f8
kasldfh <forumf41l@gmail.com>
2018-09-30 00:09:58 -0500

    updated scripts more

--- setup/vimsetup.sh
+++ setup/vimsetup.sh
@@ -2,6 +2,7 @@
 cwd=$(pwd)
 
 #First, we install the neovim
+sudo apt update
 sudo apt install curl neovim python-neovim python3-neovim python3-pip
 #TODO: use virtualenvs for the python setups
 pip3 install neovim
@@ -10,13 +11,13 @@ pip3 install neovim
 #vimPath="$cwd/../vim/.vim"
 vimrcPath="$cwd/../vim/init.vim"
 ##remove files if they already exist
-rm -rf ~/.config/nvim
+rm -rf $HOME/.config/nvim
 
 #Install plugin manager
-curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+curl -fLo $HOME/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-ln -s $vimrcPath ~/.config/nvim/init.vim
+ln -s $vimrcPath $HOME/config/nvim/init.vim
 nvim +PlugInstall +UpdateRemotePlugins +qall!
 #create directories for vim (undo, swap, prev)
 #mkdir ~/.vim/undo
--- setup/zshsetup.sh
+++ setup/zshsetup.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 #install zsh
 sudo apt install -y zsh fonts-powerline curl 
+cd $HOME
 #install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 #install spaceship
-git clone https://github.com/denysdovhan/spaceship-prompt.git "~/.oh-my-zsh/custom/themes/spaceship-prompt"
-ln -s "~/.oh-my-zsh/themes/spaceship-prompt/spaceship.zsh-theme" "~/.oh-my-zsh/themes/spaceship.zsh-theme"
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$HOME/.oh-my-zsh/custom/themes/spaceship-prompt"
+ln -s "$HOME/.oh-my-zsh/themes/spaceship-prompt/spaceship.zsh-theme" "$HOME/.oh-my-zsh/themes/spaceship.zsh-theme"
 
 #install the .zshrc
 cwd=$(pwd)
 zshrcPath="$cwd/../zsh/.zshrc"
 #remove files if they already exist
-rm -rf ~/.zshrc
-ln -s $zshrcPath ~/.zshrc
+rm -rf $HOME/.zshrc
+ln -s $zshrcPath $HOME/.zshrc
